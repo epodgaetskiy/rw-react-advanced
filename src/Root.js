94cf037bfe8002ed8a661154b5dc796a848144a6
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2022-01-03 17:25:23 +0200

    feat: 06 start

--- src/Root.js
+++ src/Root.js
@@ -3,6 +3,7 @@ import Start01 from './start/01/01.js';
 import Start03 from './start/03/03.js';
 import Start04 from './start/04/04.js';
 import Start05 from './start/05/05.js';
+import Start06 from './start/06/06.js';
 
 const Root = () => {
   return (
@@ -11,6 +12,7 @@ const Root = () => {
       <Route path="/start/03.js" element={<Start03 />} />
       <Route path="/start/04.js" element={<Start04 />} />
       <Route path="/start/05.js" element={<Start05 />} />
+      <Route path="/start/06.js" element={<Start06 />} />
     </Routes>
   );
 }
